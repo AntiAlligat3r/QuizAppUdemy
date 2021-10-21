4682055a9d9cef12f1dd355e8c97f6dbd7d3fa75
AntiAlligat3r <fallschirmjager99@gmail.com>
2021-10-21 23:33:31 +0200

    modified ending css and fixed highscores

--- end.js
+++ end.js
@@ -1,17 +1,20 @@
-const username = document.getElementById('username');
-const saveScoreBtn = document.getElementById('saveScoreBtn');
-const finalScore = document.getElementById('finalScore');
+const username = document.getElementById("username");
+const saveScoreBtn = document.getElementById("saveScoreBtn");
+const finalScore = document.getElementById("finalScore");
 
 const mostRecentScore = localStorage.getItem("mostRecentScore");
-const highScores= JSON.parse(localStorage.getItem("highScores")) || [];
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+console.log(highScores);
 
 const MAX_HIGH = 5;
 
 finalScore.innerText = mostRecentScore;
+flag = false;
 
-username.addEventListener('keyup',() =>{
-    saveScoreBtn.disabled = !username.value;
-})
+username.addEventListener("keyup",() =>{
+    if(flag == false)
+        saveScoreBtn.disabled = !username.value;
+});
 
 saveHighScore = e =>{
     e.preventDefault();
@@ -21,10 +24,15 @@ saveHighScore = e =>{
         name: username.value
     };
 
+
+
     highScores.push(score);
     highScores.sort((a,b) => b.score -a.score);
     highScores.splice(5);
 
     localStorage.setItem("highScores",JSON.stringify(highScores));
-    saveScoreBtn.disabled;
-}
+    flag = true;
+    saveScoreBtn.disabled = true;
+    username.disabled = true;
+   // window.location.assign("/");
+};
